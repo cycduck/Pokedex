b5a14710e00cfb334a72671763e03cb6901f8eca
kelly h <kho@konradgroup.com>
2020-08-20 23:02:46 -0400

    Initial Commit

---
+++ Pokedex/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Kelly Ho on 2020-08-20.
+//  Copyright © 2020 Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
